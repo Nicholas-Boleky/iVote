d3917d3fce62725d20b01da817e4f1e911cb9015
Nicholas Boleky <bolekyn@gmail.com>
2025-05-13 17:42:20 -0700

    Initial Commit

---
+++ iVoteTests/iVoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  iVoteTests.swift
+//  iVoteTests
+//
+//  Created by Nick on 5/13/25.
+//
+
+import Testing
+@testable import iVote
+
+struct iVoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
